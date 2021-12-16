310c751c43640566b69574e1fa7342a9d59c9126
Harikiran <xyz@gmail.com>
2021-12-16 07:57:44 +0530

    programs added

---
+++ File handling.py
@@ -0,0 +1,39 @@
+# f1=open('example.txt','w')
+#
+# f1.write('harikiran')
+# f1.close()
+#
+# f2=open('example.txt','r')
+# print(f2.read())
+# f2.close()
+#
+#
+# f3=open('example.txt','a')
+# f3.write('rajashekar')
+# f3.close()
+#
+# f4=open('example.txt','r')
+# print(f4.read())
+# f4.close()
+#
+# f5=open('example.txt','r')
+# print(f5.readlines())
+#
+# f1=open('example.pdf','w')
+# f1.write('hai you are accesing my file')
+#
+
+
+f1=open('example.txt','r')
+print(f1.read())
+
+
+# f2=open('example.txt','w')
+# f2.write('ramu is good boy')
+# f2.close()
+
+print(f1.read())
+f2=open('example.txt','a')
+f2.write('python is high level language')
+
+print(f1.read())
